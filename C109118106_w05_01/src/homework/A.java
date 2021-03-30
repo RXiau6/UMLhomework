0f7067d23aaf96b153970cb6ee63212ea4ad91f9
RXiau6 <50643639+RXiau6@users.noreply.github.com>
2021-03-30 21:16:31 +0800

    week05 done

---
+++ C109118106_w05_01/src/homework/A.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework;
+
+/**
+ *
+ * @author rxiau6-PC
+ */
+public abstract class A implements B {
+
+    public void A() {
+        System.out.print("ω");
+    }
+
+    public abstract void a();
+
+    public final void f() {
+        System.out.print("Δ");
+    }
+
+    public void g() {
+        System.out.print("ψ");
+    }
+    public final double zz = 0;
+}
---
+++ C109118106_w05_02/src/homework/Mammal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework;
+
+/**
+ *
+ * @author rxiau6-PC
+ */
+public class Mammal extends Animal {
+    public void eat(){System.out.println("gimme meat!");}
+    public void regulateTemperture(){System.out.println("regulating temp");}
+}
